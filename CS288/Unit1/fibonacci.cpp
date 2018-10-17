d7b38a9ee8cbbb6a284c1bf741e22b658a8b2e95
Ian Sabey <isabey@sofi.org>
2018-10-17 08:59:40 -0600

    unit1 done

---
+++ CS288/Unit1/fibonacci.cpp
@@ -0,0 +1,28 @@
+// fibonacci generator
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int length, count, first{0}, second{1}, next;
+
+    cout << "How many iterations would you like? " << endl;
+    cin >> length;
+
+    cout << "The first " << length << " iterations of the Fibonacci series are: " << endl;
+
+    for (count = 0; count < length; count++) {
+        if (count <= 1)
+            next = count;
+        else {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+        cout << next << endl;
+    }
+}
+//
+// Created by Ian Sabey on 10/17/18.
+//
