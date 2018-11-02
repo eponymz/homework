a71c1da6efdd63a9c55717e4bb59ada51055ce4e
Ian Sabey <ianfoxsabey@gmail.com>
2018-11-01 22:26:48 -0600

    changed gender to single char length

--- CS221T/Unit2/unit2fk.sql
+++ CS221T/Unit2/unit2fk.sql
@@ -5,3 +5,11 @@ FOREIGN KEY (advisor_id) REFERENCES advisors(advisor_id)
 ALTER TABLE student_classes
 ADD CONSTRAINT FK_studentClasses
 FOREIGN KEY (class_id) REFERENCES classes(class_id)
+
+ALTER TABLE students
+DROP COLUMN gender
+GO
+
+ALTER TABLE students
+ADD gender char(1) NOT NULL
+GO
