037fa30e25d089f1c9a8b244cd8237a8fa14d066
Ian Sabey <ianfoxsabey@gmail.com>
2018-12-26 22:32:07 -0700

    vertebrates committed.. now that it is turned in..

--- CS288/Unit3/Vertebrates.h
+++ CS288/Unit3/Vertebrates.h
@@ -5,61 +5,106 @@ using namespace std;
 
 class Vertebrate
 {
-// defining variables. private to ensure they are not touched unless called by the setter functions.
-private:
-    string skinCover, animalType;
-    int appendages;
-
-
 public:
+    string skinCover, animalType, appendages;
     //constructor
-    Vertebrate() {
+    Vertebrate()
+    {
         skinCover = getSkinCover();
         animalType = getAnimalType();
         appendages = getAppendages();
     };
-
-
-    // setter functions
-    void setIsReptile() {
-        skinCover = "scales";
-        animalType = "reptiles";
-        setAppendages(4);
-    };
-    void setIsMammal() {
-        skinCover = "fur/hair";
-        animalType = "mammals";
-        setAppendages(4);
-    };
-    void setIsFowl() {
-        skinCover = "feathers";
-        animalType = "birds";
-        setAppendages(2);
-    };
-    void setIsAmphibious() {
-        skinCover = "mucus";
-        animalType = "amphibians";
-        setAppendages(4);
-    };
     void setAppendages(int legs) {
-        appendages = legs;
+        int appendages = legs;
     };
-
-
-    // getter functions
     string getSkinCover() {
         return skinCover;
     };
-
-    int getAppendages() {
+    string getAppendages() {
         return appendages;
     };
-
     string getAnimalType() {
         return animalType;
+    };
+};
+
+//class Habitat
+//{
+//public:
+//    string livingEnv;
+//    Habitat()
+//    {
+//        livingEnv = getLivingEnv();
+//    }
+//
+//    string getLivingEnv() {
+//        return livingEnv;
+//    }
+//};
+//
+//// if i included the above, each derived class below would be restructured to the following
+//class Bird: public Vertebrate, public Habitat {
+//public:
+//    Bird(string birdType) {
+//        skinCover = "feathers";
+//        animalType = "birds";
+//        livingEnv = "nests, generally in trees";
+//        setAppendages(2);
+//        cout << birdType << "s are " << animalType << endl;
+//        cout << birdType << "s have " << skinCover << endl;
+//        cout << birdType << "s have " << appendages << " legs" << endl;
+//        cout << birdType << "s (for the most part) live in " << livingEnv << endl;
+//    }
+//};
+
+class Bird: public Vertebrate {
+public:
+    Bird(string birdType) {
+        skinCover = "feathers";
+        animalType = "birds";
+        setAppendages(2);
+        cout << birdType << "s are " << animalType << endl;
+        cout << birdType << "s have " << skinCover << endl;
+        cout << birdType << "s have " << appendages << " legs" << endl;
+    }
+};
+
+class Amphibian: public Vertebrate {
+public:
+    Amphibian(string amphType) {
+        skinCover = "mucus";
+        animalType = "amphibians";
+        setAppendages(4);
+        cout << amphType << "s are " << animalType << endl;
+        cout << amphType << "s have " << skinCover << endl;
+        cout << amphType << "s have " << appendages << " legs" << endl;
+    }
+};
+
+class Mammal: public Vertebrate {
+public:
+    Mammal(string mammType) {
+        skinCover = "fur/hair";
+        animalType = "mammals";
+        setAppendages(4);
+        cout << mammType << "s are " << animalType << endl;
+        cout << mammType << "s have " << skinCover << endl;
+        cout << mammType << "s have " << appendages << " legs" << endl;
+    }
+};
+
+class Reptile: public Vertebrate {
+public:
+    Reptile(string repType) {
+        skinCover = "scales";
+        animalType = "reptiles";
+        setAppendages(4);
+        cout << repType << "s are " << animalType << endl;
+        cout << repType << "s have " << skinCover << endl;
+        cout << repType << "s have " << appendages << " legs" << endl;
     }
 };
 
 //
-// Created by Ian Sabey on 11/29/18.
+// Created by Ian Sabey on 12/24/18.
 //
