950e53c2f1803a633b67cadc7bf2ddc9282dd7a7
Ian Sabey <ianfoxsabey@gmail.com>
2018-12-26 22:33:31 -0700

    unit3done

--- CS288/Unit3/main.cpp
+++ CS288/Unit3/main.cpp
@@ -2,36 +2,81 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include "Vehicle.h"
 #include "Vertebrates.h"
 
 using namespace std;
 
-int main() {
-    cout << "Welcome to Unit 3. Which file would you like to run? (string/construct)" << endl;
+string getUserChoice() {
+    string userResponse, userRespType;
+    cout << "What kind of vertebrate would you like info on? "
+            "birds/mammals/reptiles/amphibians" << endl;
+    cin >> userResponse;
+    if (userResponse == "birds") {
+        // secondary question to make the program seem more user friendly
+        cout << "What kind of bird?" << endl;
+        cin >> userRespType;
+        new Bird(userRespType);
+    } else if (userResponse == "mammals") {
+        // secondary question to make the program seem more user friendly
+        cout << "What kind of mammal?" << endl;
+        cin >> userRespType;
+        new Mammal(userRespType);
+    } else if (userResponse == "reptiles") {
+        // secondary question to make the program seem more user friendly
+        cout << "What kind of reptile?" << endl;
+        cin >> userRespType;
+        new Reptile(userRespType);
+    } else if (userResponse == "amphibians") {
+        // secondary question to make the program seem more user friendly
+        cout << "What kind of amphibian?" << endl;
+        cin >> userRespType;
+        new Amphibian(userRespType);
+    } else {
+        cout << "Please enter an option from above.";
+        getUserChoice();
+    }
+    return std::string();
+};
+
+void runVehicle() {
+    string userChoice;
+    string vehicleTypes[5] = {"sedan","minivan","crossover","coupe","convertible"};
+    cout << "What kind of vehicle would you like details on? "
+            "(sedan/minivan/crossover/coupe/convertible)" << endl;
+    cin >> userChoice;
+    if (userChoice == vehicleTypes[0]) {
+        new Sedan();
+    } else if (userChoice == vehicleTypes[1]) {
+        new Minivan();
+    } else if (userChoice == vehicleTypes[2]) {
+        new Crossover();
+    } else if (userChoice == vehicleTypes[3]) {
+        new Coupe();
+    } else if (userChoice == vehicleTypes[4]) {
+        new Convertible();
+    } else {
+        cout << "Invalid option entered..\nReturning to main.." << endl;
+    }
+}
+
+int main()
+{
     string fileChoice;
+    cout << "Which file would you like to run? (vehicle/vertebrates)" << endl;
     cin >> fileChoice;
-    if (fileChoice == "vehicle") {
-        cout << "Not completed yet.." << endl;
+    if (fileChoice == "vertebrates") {
+        getUserChoice();
         main();
-    }
-    else if (fileChoice == "construct") {
-        cout << "Let's build a lizard." << endl;
-        Vertebrate lizard;
-        lizard.setIsReptile();
-        string lizardSkin = lizard.getSkinCover();
-        string lizardType = lizard.getAnimalType();
-        int lizardLegs = lizard.getAppendages();
-        cout << "Lizards are " << lizardType << ", they have " << lizardSkin << " covering their skin, and they have "
-        << lizardLegs << " legs." << endl;
+    } else if (fileChoice == "vehicle") {
+        runVehicle();
         main();
-    }
-    else
-    {
-        cout << "That is an invalid option" << endl;
+    } else {
+        cout << "Not a valid option.." << endl;
         main();
     }
 }
 
 //
-// Created by Ian Sabey on 10/17/18.
+// Created by Ian Sabey on 12/24/18.
 //
